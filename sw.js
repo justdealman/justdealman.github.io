3e8e257a4bf6e271f5ac8bbe8b293a9d14bb1d13
justdealman <justdealman@gmail.com>
2024-05-07 21:18:22 +0700

    b

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('push', (event) => {
+    event.waitUntil(
+        self.registration.showNotification('Notification Title', {
+            body: 'Notification Body Text',
+            icon: 'custom-notification-icon.png',
+        })
+    );
+});
